32bd1baca431eb5eca9087e2d49e294fe05ce730
nilofe <nyucragavilan@gmail.com>
2023-06-15 05:57:26 +0000

    update .gitignore, apply terraform test

--- az-tf/provider.tf
+++ az-tf/provider.tf
@@ -1,12 +1,21 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.61.0"
     }
   }
+
+  required_version = ">=1.1.0"
 }
 
 provider "azurerm" {
   # Configuration options
+  features {}
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTestResourceGroupMany"
+  location = "westus2"
+}
+
